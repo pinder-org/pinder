d3596cfc5495daaea9f8bd5e70bc2a451b8dd6c2
danielkovtun <danny@vant.ai>
2024-07-22 11:37:00 -0400

    chore: expose BiotiteDockQ in top-level pinder-eval package

--- src/pinder-eval/pinder/eval/__init__.py
+++ src/pinder-eval/pinder/eval/__init__.py
@@ -1,5 +1,9 @@
 """Namespace package root for pinder-eval."""
 
 from pinder.eval._version import _get_version
+from pinder.eval.dockq.biotite_dockq import BiotiteDockQ
 
 __version__ = _get_version()
+
+
+__all__ = ["BiotiteDockQ"]
