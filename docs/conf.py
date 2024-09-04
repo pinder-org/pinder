d73a3220de4dbabfddc4716ef837390bacfd0e1e
danielkovtun <danny@vant.ai>
2024-09-04 16:17:18 -0400

    docs: facelift

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,21 @@
 
 import sys
 from os.path import realpath, dirname, join
+import os
+import sys
+from pathlib import Path
+
+import pinder
+
+DOC_PATH = Path(__file__).parent
+
+# Avoid verbose logs in rendered notebooks
+os.environ["PINDER_LOG_LEVEL"] = "0"
+
+# Include documentation in PYTHONPATH
+# in order to import modules for API doc generation etc.
+sys.path.insert(0, str(DOC_PATH))
+import viewcode
 
 doc_path = dirname(realpath(__file__))
 package_path = join(dirname(doc_path), "src")
@@ -43,17 +58,20 @@ version = get_release()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "jupyter_sphinx",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.doctest",
     "sphinx.ext.mathjax",
-    # "sphinx.ext.linkcode",
+    "sphinx.ext.linkcode",
     # "sphinxcontrib.bibtex",
     "numpydoc",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx.ext.githubpages",
+    "sphinx_design",
+    "sphinx_copybutton",
     "myst_nb",
 ]
 
@@ -113,13 +131,17 @@ myst_enable_extensions = [
 
 myst_url_schemes = ("http", "https", "mailto")
 
+linkcode_resolve = viewcode.linkcode_resolve
 
+project = "PINDER"
+copyright = "2024, PINDER Development Team"
+author = "PINDER Development Team"
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.
 
 #### HTML ####
-html_theme = "piccolo_theme"
+html_theme = "pydata_sphinx_theme"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -127,14 +149,13 @@ html_theme = "piccolo_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_logo = "_static/pinder.png"
-html_theme_options = {
-    "source_url": "https://github.com/pinder-org/pinder/",
-    "source_icon": "github",
-    "show_theme_credit": False,
-}
-
 html_css_files = [
     "custom.css",
+    # Get fonts from Google Fonts CDN
+    "https://fonts.googleapis.com/css2"
+    "?family=Geologica:wght@100..900"
+    "&family=Montserrat:ital,wght@0,100..900;1,100..900"
+    "&display=swap",
 ]
 html_js_files = [
     "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"
@@ -142,3 +163,41 @@ html_js_files = [
 
 html_favicon = "_static/favicon.ico"
 htmlhelp_basename = "PinderDoc"
+html_baseurl = "https://pinder-org.github.io/pinder/"
+
+html_theme_options = {
+    "header_links_before_dropdown": 7,
+    "pygments_light_style": "friendly",
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pinder-org/pinder/",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "Article",
+            "url": "https://www.biorxiv.org/content/10.1101/2024.07.17.603980v4",
+            "icon": "fa-solid fa-file-lines",
+            "type": "fontawesome",
+        },
+    ],
+    "use_edit_page_button": True,
+    "show_prev_next": False,
+    "show_toc_level": 2,
+}
+html_sidebars = {
+    # No primary sidebar for these pages
+    "readme": [],
+    "example_readme": [],
+    "faq": [],
+    "limitations": [],
+    "changelog": [],
+}
+html_context = {
+    "github_user": "pinder-org",
+    "github_repo": "pinder",
+    "github_version": "main",
+    "doc_path": "doc",
+}
+html_scaled_image_link = False
