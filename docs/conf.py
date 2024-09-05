48725f23d263460903c28b878eba07286f0ca023
danielkovtun <danny@vant.ai>
2024-09-05 18:35:58 -0400

    bumpversion minor

--- docs/conf.py
+++ docs/conf.py
@@ -144,9 +144,6 @@ author = "PINDER Development Team"
 html_theme = "pydata_sphinx_theme"
 
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_logo = "_static/pinder.png"
 html_css_files = [
