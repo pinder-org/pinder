7c5670b233ea0c20745cd3c8d67e2a60447b50c9
Mehmet Akdel <akdel.mehmet@gmail.com>
2024-09-23 15:13:20 +0000

    added  ()

--- src/pinder-core/pinder/core/loader/transforms.py
+++ src/pinder-core/pinder/core/loader/transforms.py
@@ -1,5 +1,7 @@
 from pinder.core.index.system import PinderSystem
 from pinder.core.loader.structure import Structure
+from scipy.spatial.transform import Rotation as R
+import numpy as np
 
 
 class TransformBase:
@@ -57,3 +59,37 @@ class SuperposeToReference(TransformBase):
                 unbound_super, _, _ = L_struc.superimpose(L_ref)
                 setattr(ppi, L_monomer, unbound_super)
         return ppi
+
+
+class RandomLigandTransform(TransformBase):
+    def __init__(self, max_translation: float = 10.0) -> None:
+        self.max_translation = max_translation
+
+    def transform(self, ppi: PinderSystem) -> PinderSystem:
+        assert ppi.entry.holo_L and ppi.entry.holo_L
+        ppi = self.transform_struct(
+            ppi, R.random().as_matrix(), np.random.randn(3) * self.max_translation
+        )
+        return ppi
+
+    @staticmethod
+    def transform_struct(pinder_system, rotation_matrix, translation_vector):
+        centroid = pinder_system.atom_array.coord.mean(axis=0)
+        pinder_system.atom_array.coord = pinder_system.atom_array.coord - centroid
+        pinder_system.atom_array.coord = (
+            rotation_matrix @ pinder_system.atom_array.coord.T
+        ).T
+        pinder_system.atom_array.coord = (
+            pinder_system.atom_array.coord + centroid + translation_vector
+        )
+        for L_monomer in ["apo_ligand", "pred_ligand"]:
+            L_struc = getattr(pinder_system, L_monomer)
+            if L_struc:
+                L_struc.atom_array.coord = L_struc.atom_array.coord - centroid
+                L_struc.atom_array.coord = (
+                    rotation_matrix @ L_struc.atom_array.coord.T
+                ).T
+                L_struc.atom_array.coord = (
+                    L_struc.atom_array.coord + centroid + translation_vector
+                )
+        return pinder_system
